1c7f5400d1775fe123e077719c0488e8a104a7bb
mariasanzs <mariasanzsanchez45@gmail.com>
2022-04-14 19:32:01 +0200

    uploading script for generating g and p #1

--- code/generateGP.py
+++ code/generateGP.py
@@ -1,4 +1,9 @@
+import random
 class GPGenerator:
 	def generateGP():
-		# ToDo
-		return (18, 31)
+		# g is a small prime number
+		small_prime_numbers = [2,3,5,7,11,13,17,19,23,29,31,37,41]
+		g = random.choice(small_prime_numbers)
+		#p is a big (2000 bits) number
+		p = random.getrandbits(2000)
+		return (g, p)
